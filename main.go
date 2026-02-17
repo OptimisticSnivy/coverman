1c36ef9a21bec7acbd16e218816abf8e95d64e14
Anand Mahamuni <optimisticsnivy7203@proton.me>
2026-02-17 23:36:31 +0530

    ignore resized output images

--- main.go
+++ main.go
@@ -25,25 +25,41 @@ func walk(s string, d fs.DirEntry, err error) error {
 	return nil
 }
 
+func Map(x int, in_min int, in_max int, out_min int, out_max int) int {
+	return ((x-in_min)*(out_max-out_min)/(in_max-in_min) + out_min)
+}
+
 func main() {
 	arg := os.Args[1:]
+	var pixels []int
+	var density string = ".:-=+*#%@"
 
 	filepath.WalkDir(arg[0], walk)
-	fmt.Println(files[0])
 
-	f, err := os.Open(files[2])
+	f, err := os.Open(files[3])
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	img, _, err := image.Decode(f)
-	r := resize.Resize(60, 0, img, resize.Lanczos3)
-
-	resized, err := os.Create("resized.jpg")
-	if err != nil {
-		log.Fatal(err)
+	img, _, error := image.Decode(f)
+	if error != nil {
+		log.Fatal(error)
 	}
 
-	jpeg.Encode(resized, r, nil)
+	rf := resize.Resize(60, 0, img, resize.Lanczos3)
+	for i := 0; i < rf.Bounds().Dx(); i++ {
+		for j := 0; j < rf.Bounds().Dy(); j++ {
+			r, g, b, _ := rf.At(j, i).RGBA()
+			br := int(r>>8+g>>8+b>>8) / 3
+			brm := Map(br, 0, 255, 0, 9)
+			pixels = append(pixels, brm)
+			fmt.Print(string(density[brm]))
+		}
+		fmt.Print("\n")
+	}
+
+	resized, _ := os.Create("resized.jpg")
+
+	jpeg.Encode(resized, rf, nil)
 	defer f.Close()
 }
